1a8f2350f2582d397caf64d3122b7cc2e78755cd
pkrisko <patrick.krisko@gmail.com>
2020-08-02 10:23:35 -0500

    add favicon

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Head from 'next/head';
 import { ProvideAuth } from '../util/auth';
 import '../styles/app.css';
 import 'react-piano/dist/styles.css';
 
 const App = ({Component, pageProps}) => (
     <ProvideAuth>
+        <Head>
+            <link rel="stylesheet" href="/public/favicon.ico"/>
+        </Head>
         <Component {...pageProps} />
     </ProvideAuth>
 );
