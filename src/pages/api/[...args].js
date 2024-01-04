d7da53e6a07901d4dd18d7b0a42d6c2dfdca9288
pkrisko <patrick.krisko@gmail.com>
2024-01-03 23:16:42 -0500

    Send '...args'

--- src/pages/api/[...args].js
+++ src/pages/api/[...args].js
@@ -11,9 +11,11 @@ export const config = {
 
 const handler = (req, res) => new Promise((resolve, reject) => {
   req.headers['x-forwarded-port'] = req.headers['x-forwarded-port'] || '443';
+  const target = `${API_URL}/${req.query.args}`;
   console.log('API_URL', API_URL); // eslint-disable-line no-console
+  console.log('targetPath', target); // eslint-disable-line no-console
   console.log('Authorization Header:', req.headers.authorization); // eslint-disable-line no-console
-  proxy.web(req, res, { target: API_URL, changeOrigin: true, prependPath: true }, (err) => {
+  proxy.web(req, res, { target, changeOrigin: true, prependPath: true }, (err) => {
     if (err) {
       console.error(err); // eslint-disable-line no-console
       res.status(500).json({ error: 'Error occurred in proxy' });
